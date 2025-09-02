e939c72a59f152fb61ef800a23d382c72987d27a
Bieoidungbuonnua <166198515+Bieoidungbuonnua@users.noreply.github.com>
2025-09-01 23:16:30 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -33,3 +33,6 @@ cd /sdcard/download
 wget --no-check-certificate 'https://drive.usercontent.google.com/download?id=1rz4aLqoQek987SxwT_SYrSmWvMtwqNoj&export=download&authuser=0&confirm=t&uuid=f3f50774-f8a5-49d8-a10e-85661641db44&at=AN8xHooxidRL0DO7aAWmBYHKlUP0%3A1756640452122' -O gl.py
 wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=14Cjz0LZCgwW8Dlw0JL7f9ObZhkjFiU3U&confirm=t' -O Authorization.txt
 wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1aP20mGtdCeS9a6O8DiLmaYwGdL_pg9PY&confirm=t' -O token.txt
+
+# Chạy tool
+python gl.py
